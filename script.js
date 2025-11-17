2942d14bfea0605b6340b8038aa5d209347ab31f
federicosimone <federico.simone15@gmail.com>
2025-11-17 20:11:15 +0100

    sintetizzazione

--- script.js
+++ script.js
@@ -4,21 +4,19 @@ let serieA = [
     {nome: "Pisa", puntiFatti: "" , falliSubiti: "" } ,
     {nome: "Cremonese", puntiFatti: "" , falliSubiti: "" }
 ]
-
+ 
+let elencoSquadre= []
 
 for (i=0 ; i< serieA.length ; i++) {
     serieA[i].puntiFatti= Math.floor((Math.random() * 100) + 1) ;
     serieA[i].falliSubiti= Math.floor((Math.random() * 10) + 1) ;
+    elencoSquadre.push(serieA[i].nome) ;
 }
 
 console.log(serieA)
-
-let elencoSquadre= []
-
-for (i=0 ; i< serieA.length ; i++) {
-
-    elencoSquadre.push(serieA[i].nome) ;
-
-}
-
 console.log(elencoSquadre)
+
+//pushare nomi 
+
+
+
