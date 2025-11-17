79c2cbae52b1c0cc56882e2f56b6f22338c53c1d
federicosimone <federico.simone15@gmail.com>
2025-11-17 18:39:03 +0100

    tentativo1

--- main.js
+++ main.js
@@ -4,4 +4,21 @@ let biciDaCorsa = [
     {nome:"Pinarello" , peso: "13"} ,  
 ]
 
-console.log(biciDaCorsa)
+//console.log(biciDaCorsa)
+
+//console.log(parseInt(biciDaCorsa[2].peso))
+
+pesoMinore= null
+
+
+for (i=0; i< biciDaCorsa.length ; i++) {
+
+    let pesoBici= parseFloat(biciDaCorsa[i].peso)
+
+
+    if (pesoBici < parseFloat(biciDaCorsa[i+1]) ) {
+        pesoMinore = pesoBici ;
+    } /*else if (pesoBici < parseFloat(biciDaCorsa[i-1].peso)) {
+        console.log(parseFloat(biciDaCorsa[i].peso))
+    }*/
+}
