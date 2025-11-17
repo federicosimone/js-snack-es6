0d259004151e0cea8ee545ce58e1e9a611c065da
federicosimone <federico.simone15@gmail.com>
2025-11-17 19:58:23 +0100

    tentativo1

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const serieA = [
+let serieA = [
     {nome: "Juventus", puntiFatti: "" , falliSubiti: "" } ,
     {nome: "Roma", puntiFatti: "" , falliSubiti: "" } ,
     {nome: "Pisa", puntiFatti: "" , falliSubiti: "" } ,
     {nome: "Cremonese", puntiFatti: "" , falliSubiti: "" }
 ]
+
+
+for (i=0 ; i< SerieA.length ; i++) {
+    serieA[i].puntiFatti= Math.floor((Math.random() * 100) + 1) ;
+}
