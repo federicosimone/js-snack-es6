1995ad2c2106b80bc4a4150cb0f833ef6a657bce
federicosimone <federico.simone15@gmail.com>
2025-11-17 20:25:24 +0100

    end2

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let serieA = [
  
 let elencoSquadre= []
 
+
 for (i=0 ; i< serieA.length ; i++) {
     serieA[i].puntiFatti= Math.floor((Math.random() * 100) + 1) ;
     serieA[i].falliSubiti= Math.floor((Math.random() * 10) + 1) ;
