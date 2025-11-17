2105607b3af43da0cf369195b846d899f564cf11
federicosimone <federico.simone15@gmail.com>
2025-11-17 19:18:29 +0100

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const serieA = [
-    {nome: "Juventus", puntiFatti: "" , falliSubiti: "" } ;
-    {nome: "Roma", puntiFatti: "" , falliSubiti: "" } ;
-    {nome: "Pisa", puntiFatti: "" , falliSubiti: "" } ;
+    {nome: "Juventus", puntiFatti: "" , falliSubiti: "" } ,
+    {nome: "Roma", puntiFatti: "" , falliSubiti: "" } ,
+    {nome: "Pisa", puntiFatti: "" , falliSubiti: "" } ,
     {nome: "Cremonese", puntiFatti: "" , falliSubiti: "" }
 ]
