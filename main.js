bbe651b1d45bd045f693e3e40fb8dea44f0ff242
federicosimone <federico.simone15@gmail.com>
2025-11-17 19:02:26 +0100

    tentativo 2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let biciDaCorsa = [
-    {nome:"Bianchi" , peso: "6,8"} , 
-    {nome:"Triban" , peso: "7,2"} ,
+    {nome:"Bianchi" , peso: "10.5"} , 
+    {nome:"Triban" , peso: "7.2"} ,
     {nome:"Pinarello" , peso: "13"} ,  
 ]
 
@@ -8,17 +8,17 @@ let biciDaCorsa = [
 
 //console.log(parseInt(biciDaCorsa[2].peso))
 
-pesoMinore= null
+let pesoMinore= biciDaCorsa[0].peso ;
 
 
-for (i=0; i< biciDaCorsa.length ; i++) {
+for (i=1; i< biciDaCorsa.length ; i++) {
 
     let pesoBici= parseFloat(biciDaCorsa[i].peso)
 
-
-    if (pesoBici < parseFloat(biciDaCorsa[i+1]) ) {
-        pesoMinore = pesoBici ;
-    } /*else if (pesoBici < parseFloat(biciDaCorsa[i-1].peso)) {
-        console.log(parseFloat(biciDaCorsa[i].peso))
-    }*/
+    if (pesoBici < parseFloat(biciDaCorsa[i-1].peso)) {
+        pesoMinore = parseFloat(pesoBici) ;
+        
+    } 
 }
+
+console.log(pesoMinore);
